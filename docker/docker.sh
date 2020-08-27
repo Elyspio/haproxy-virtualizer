56da2d9954ca695298c15778cbceac0ea5f10b67
Jonathan Guichard <jona.guich69@gmail.com>
2020-08-27 09:21:46 +0200

    Docker update

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 origin=$(pwd)
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo $DIR
-cd $DIR
 
 
 rm -rdf "$DIR/../back/build"
@@ -21,7 +21,7 @@ else
 fi
 
 echo "Building for amd64"
-"$DIR/amd64.sh"
+"$DIR/adm/amd64.sh"
 
 echo "Building for arm64"
 "$DIR/arm/arm64.sh"
---
+++ docker/variables.sh
@@ -0,0 +1,1 @@
+export app="haproxy-virtualizer"
